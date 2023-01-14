b1a309a1e1df4dc38c10dbc503cc902cfaa47c8c
nveki <kevin.foucret@gmail.com>
2023-01-14 19:00:26 +0100

    project ready to start

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html.twig',    
+    './node_modules/tw-elements/dist/js/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+  ],
+}
